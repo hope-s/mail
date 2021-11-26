27a0dbbc5c4ba35f93bbb3a158018200b32ef37c
Sobhan <sobhanyazdanjo@gmail.com>
2021-11-26 11:03:40 +0330

    Edited left Menu, Deleted setting icon, Made responsive

---
+++ src/app/fuse-configs/navigationConfig.js
@@ -0,0 +1,17 @@
+
+const navigationConfig = [
+  {
+    id: "chat",
+    title: "Chat",
+    type: "item",
+    icon: "chat",
+    url: "/apps/chat",
+    badge: {
+      title: 13,
+      bg: "rgb(9, 210, 97)",
+      fg: "#FFFFFF",
+    },
+  },
+];
+
+export default navigationConfig;
