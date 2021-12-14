9d9bf00b443718dcf187caf8f1f44a374c66329a
Sobhan <sobhanyazdanjo@gmail.com>
2021-12-14 21:36:18 +0330

    Added Login and Register api and front.

--- src/app/main/login/tabs/JWTLoginTab.js
+++ src/app/main/login/tabs/JWTLoginTab.js
@@ -1,103 +1,121 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Button, Divider, Typography, InputAdornment, Icon} from '@material-ui/core';
-import {TextFieldFormsy} from '@fuse';
-import Formsy from 'formsy-react';
-import * as authActions from 'app/auth/store/actions';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useEffect, useRef, useState } from "react";
+import { Button, InputAdornment, Icon } from "@material-ui/core";
+import { TextFieldFormsy } from "@fuse";
+import Formsy from "formsy-react";
+import { useSelector } from "react-redux";
+import LoginUseQuery from "core/services/api/Login.api";
+import { useHistory } from "react-router-dom";
+import { showToast } from "core/utils/show-toast";
 
-function JWTLoginTab(props)
-{
-    const dispatch = useDispatch();
-    const login = useSelector(({auth}) => auth.login);
+function JWTLoginTab(props) {
+  //   const dispatch = useDispatch();
+  const login = useSelector(({ auth }) => auth.login);
 
-    const [isFormValid, setIsFormValid] = useState(false);
-    const formRef = useRef(null);
-
-    useEffect(() => {
-        if ( login.error && (login.error.email || login.error.password) )
-        {
-            formRef.current.updateInputsWithError({
-                ...login.error
-            });
-            disableButton();
-        }
-    }, [login.error]);
-
-    function disableButton()
-    {
-        setIsFormValid(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+  const formRef = useRef(null);
+  const loginQuery = LoginUseQuery();
+  let history = useHistory();
+  useEffect(() => {
+    if (login.error && (login.error.email || login.error.password)) {
+      formRef.current.updateInputsWithError({
+        ...login.error,
+      });
+      disableButton();
     }
+  }, [login.error]);
 
-    function enableButton()
-    {
-        setIsFormValid(true);
-    }
+  function disableButton() {
+    setIsFormValid(false);
+  }
 
-    function handleSubmit(model)
-    {
-        dispatch(authActions.submitLogin(model));
-    }
+  function enableButton() {
+    setIsFormValid(true);
+  }
 
-    return (
-        <div className="w-full">
-            <Formsy
-                onValidSubmit={handleSubmit}
-                onValid={enableButton}
-                onInvalid={disableButton}
-                ref={formRef}
-                className="flex flex-col justify-center w-full"
-            >
-                <TextFieldFormsy
-                    className="mb-16"
-                    type="text"
-                    name="email"
-                    label="Username/Email"
-                    validations={{
-                        minLength: 4
-                    }}
-                    validationErrors={{
-                        minLength: 'Min character length is 4'
-                    }}
-                    InputProps={{
-                        endAdornment: <InputAdornment position="end"><Icon className="text-20" color="action">email</Icon></InputAdornment>
-                    }}
-                    variant="outlined"
-                    required
-                />
+  function handleSubmit(model) {
+    loginQuery.mutate(
+      { username: model.email, password: model.password },
+      {
+        onSuccess: (result) => {
+          showToast(["Login was successful"], "success");
+          localStorage.setItem("token", result.data.data.token);
+          history.push("/apps/chat");
+        },
+      }
+    );
+  }
 
-                <TextFieldFormsy
-                    className="mb-16"
-                    type="password"
-                    name="password"
-                    label="Password"
-                    validations={{
-                        minLength: 4
-                    }}
-                    validationErrors={{
-                        minLength: 'Min character length is 4'
-                    }}
-                    InputProps={{
-                        endAdornment: <InputAdornment position="end"><Icon className="text-20" color="action">vpn_key</Icon></InputAdornment>
-                    }}
-                    variant="outlined"
-                    required
-                />
+  return (
+    <div className="w-full">
+      <Formsy
+        onValidSubmit={handleSubmit}
+        onValid={enableButton}
+        onInvalid={disableButton}
+        ref={formRef}
+        className="flex flex-col justify-center w-full"
+      >
+        <TextFieldFormsy
+          className="mb-16"
+          type="text"
+          name="email"
+          label="Username/Email"
+          validations={{
+            minLength: 4,
+          }}
+          validationErrors={{
+            minLength: "Min character length is 4",
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Icon className="text-20" color="action">
+                  email
+                </Icon>
+              </InputAdornment>
+            ),
+          }}
+          variant="outlined"
+          required
+        />
 
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    className="w-full mx-auto mt-16 normal-case"
-                    aria-label="LOG IN"
-                    disabled={!isFormValid}
-                    value="legacy"
-                >
-                    Login
-                </Button>
+        <TextFieldFormsy
+          className="mb-16"
+          type="password"
+          name="password"
+          label="Password"
+          validations={{
+            minLength: 4,
+          }}
+          validationErrors={{
+            minLength: "Min character length is 4",
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Icon className="text-20" color="action">
+                  vpn_key
+                </Icon>
+              </InputAdornment>
+            ),
+          }}
+          variant="outlined"
+          required
+        />
 
-            </Formsy>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          className="w-full mx-auto mt-16 normal-case"
+          aria-label="LOG IN"
+          disabled={!isFormValid}
+          value="legacy"
+        >
+          Login
+        </Button>
+      </Formsy>
 
-            <div className="flex flex-col items-center pt-24">
+      {/* <div className="flex flex-col items-center pt-24">
                 <Typography className="text-14 font-600 py-8">
                     Credentials
                 </Typography>
@@ -125,10 +143,9 @@ function JWTLoginTab(props)
                         </tr>
                     </tbody>
                 </table>
-            </div>
-
-        </div>
-    );
+            </div> */}
+    </div>
+  );
 }
 
 export default JWTLoginTab;
---
+++ src/core/services/api/Login.api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { useMutation } from "react-query";
+
+const mainURL = process.env.REACT_APP_BASE_URL;
+const loginUrl = mainURL + "auth/login/username/user/";
+
+const loginQuery = async (value) => {
+  // value is the data coming from form
+
+  return axios.post(loginUrl, value);
+};
+
+const LoginUseQuery = () => {
+  return useMutation(loginQuery); //// useMutation for post, delete, update requests
+};
+
+export default LoginUseQuery;
